e002b2f34c1ec5e8cb1c44a4a43317112181482f
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 17:21:41 +0200

    change student to JPA "table in DB"

--- src/main/java/com/example/demo/student/Student.java
+++ src/main/java/com/example/demo/student/Student.java
@@ -1,13 +1,39 @@
 package com.example.demo.student;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+
+@Entity
+@Table
 public class Student {
+
+    @Id
+    @SequenceGenerator(
+            name="student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_sequence"
+    )
     private Long id;
     private String name;
     private String email;
     private LocalDate dob;
     private Integer age;
+
+    public Student() {
+
+    }
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
 
     public Student(Long id, String name, String email, LocalDate dob, Integer age) {
         this.id = id;
@@ -17,12 +43,6 @@ public class Student {
         this.age = age;
     }
 
-    public Student(String name, String email, LocalDate dob, Integer age) {
-        this.name = name;
-        this.email = email;
-        this.dob = dob;
-        this.age = age;
-    }
 
     public Long getId() {
         return id;
