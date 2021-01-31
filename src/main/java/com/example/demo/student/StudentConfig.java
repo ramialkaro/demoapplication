cae7a8fd1efe9f7152dd5462d0ca52a538104024
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 17:39:45 +0200

    create two student as example

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,31 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+        return args -> {
+            Student rami = new Student(
+                    "Rami",
+                    "rami@gmail.com",
+                    LocalDate.of(1993, Month.FEBRUARY, 7),
+                    28);
+            Student ali = new Student(
+                    "ali",
+                    "ali@gmail.com",
+                    LocalDate.of(1969, Month.FEBRUARY, 7),
+                    28);
+
+            studentRepository.saveAll(List.of(rami, ali));
+        };
+    }
+}
