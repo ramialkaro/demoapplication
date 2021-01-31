428ff496962c056ed9dbabb6d3bdc7a45dc91965
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 18:04:39 +0200

    add the query to find student by email so we could check if the student is exist throw error otherwise will be save to db

--- src/main/java/com/example/demo/student/StudentRepository.java
+++ src/main/java/com/example/demo/student/StudentRepository.java
@@ -1,8 +1,16 @@
 package com.example.demo.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+
+    // SELECT * FROM Student where email is email
+
+    @Query("SELECT s FROM Student s where s.email =?1")
+    Optional<Student> findStudentByEmail(String email);
 }
