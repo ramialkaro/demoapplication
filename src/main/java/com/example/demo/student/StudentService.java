f9124b7851b622dcce1675c148b9a7a61516506d
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 18:56:39 +0200

    using the Transactional notation so no need to write any query. Just the getter and setter will update DTO. implement the updateStudent method for the StudentService.

--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -2,9 +2,11 @@ package com.example.demo.student;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -38,5 +40,25 @@ public class StudentService {
             );
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long id, String name, String email){
+        Student student = studentRepository.findById(id)
+                .orElseThrow(()-> new IllegalStateException("student with id "+ id + "dose not exists"));
+        if(name !=null
+                && name.length() > 0
+                && Objects.equals(student.getName(), name)){
+            student.setName(name);
+        }
+        if(email !=null
+                && email.length() > 0
+                && Objects.equals(student.getEmail(), email)){
+            Optional<Student> existsStudent = studentRepository.findStudentByEmail(email);
+            if(existsStudent.isPresent()){
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
     }
 }
