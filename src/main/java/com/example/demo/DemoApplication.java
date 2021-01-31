15d47512f91e17e1531eaced5fceefa963ffabd2
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 14:35:14 +0200

    change the return of the API to be list of student.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,23 +1,28 @@
 package com.example.demo;
 
+import com.example.demo.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@GetMapping("")
-	public List<String> hello(){
-		return List.of("hello"," Rami from Demo application");
-	}
+    @GetMapping("")
+    public List<Student> hello() {
+        return List.of(
+                new Student(1L, "Rami", "rami@gmail.com", LocalDate.of(1993, Month.FEBRUARY, 7), 28)
+        );
+    }
 
 }
