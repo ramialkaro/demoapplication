6b6784bc1cf43d59a5cf0858eaf7455acf50e63c
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 14:49:14 +0200

    move logic to service layer and use the dependency injection with connect to service layer

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -1,22 +1,26 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/student")
 public class StudentController {
 
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
     @GetMapping
-    public List<Student> getStudent() {
-        return List.of(
-                new Student(1L, "Rami", "rami@gmail.com", LocalDate.of(1993, Month.FEBRUARY, 7), 28)
-        );
+    public List<Student> getStudents() {
+        return studentService.getStudents();
     }
 
 }
