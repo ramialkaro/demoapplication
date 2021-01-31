e4415b926e8e127fecba63501d18e5e2343ca84f
ramialkaro <rami.alkaro@gmail.com>
2021-01-31 21:37:28 +0200

    implement course model

---
+++ src/main/java/com/example/demo/model/Course.java
@@ -0,0 +1,58 @@
+package com.example.demo.model;
+
+
+import java.time.LocalDate;
+
+public class Course {
+
+    private Long id;
+    private String name;
+    private LocalDate createdAt;
+
+    public Course() {
+    }
+
+    public Course(Long id, String name, LocalDate createdAt) {
+        this.id = id;
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Course(String name, LocalDate createdAt) {
+        this.name = name;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
